90e6cb64d7dffa83fd70748b041de573ac1c52d2
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-21 18:36:32 +0100

    printed on page with pictures and correct layout

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-/*
-Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
-Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
-MILESTONE 1:
-Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-MILESTONE 2:
-Stampare le stesse informazioni su DOM sottoforma di stringhe
-BONUS 1:
-Trasformare la stringa foto in una immagine effettiva
-BONUS 2:
-Organizzare i singoli membri in card/schede e rendere la pagina gradevole (potete usare lo screen in allegato come spunto
-Consigli del giorno:
-Ragioniamo come sempre a step.
-Prima la logica in italiano e poi traduciamo in codice.
-E ricordiamoci che console.log() è nostro amico!
-Buon lavoro!
-*/
-
 //* DOMS ELEMENTS
 const teamMembers = document.getElementById('team-members');
 
@@ -27,32 +7,32 @@ const infoTeamArray = [
     {
         name : 'Wayne Barnet',
         role : 'Founder & CEO',
-        image : 'wayne-barnett-founder-ceo.jpg',
+        image:'img/wayne-barnett-founder-ceo.jpg',
     },
     {
         name : 'Angela Caroll',
         role : 'Chief Editor',
-        image : 'angela-caroll-chief-editor.jpg',
+        image:'img/angela-caroll-chief-editor.jpg',
     },
     {
         name : 'Walter Gordon',
         role : 'Office Manager',
-        image : 'walter-gordon-office-manager.jpg',
+        image:'img/walter-gordon-office-manager.jpg',
     },
     {
         name : 'Angela Lopez',
         role : 'Social Media Manager',
-        image : 'angela-lopez-social-media-manager.jpg',
+        image:'img/angela-lopez-social-media-manager.jpg',
     },
     {
         name : 'Scott Estrada',
         role : 'Developer',
-        image : 'scott-estrada-developer.jpg',
+        image : 'img/scott-estrada-developer.jpg',
     },
     {
         name : 'Barbara Ramos',
         role : 'Graphic Designer',
-        image : 'barbara-ramos-graphic-designerbarbara-ramos-graphic-designer.jpg',
+        image:'img/barbara-ramos-graphic-designer.jpg',
     }
 ];
 
@@ -61,5 +41,12 @@ for (let i = 0; i < infoTeamArray.length; i++){
     console.log(infoTeam.name);
     console.log(infoTeam.role);
     console.log(infoTeam.image);
-    teamMembers.innerHTML += `${infoTeam.name} ${infoTeam.role} ${infoTeam.image}`;
+    teamMembers.innerHTML +=
+    `<div class="col">
+        <div class="card">
+            <img src="${infoTeam.image}" alt="${infoTeam.image}">
+            <h3>${infoTeam.name}</h3>
+            <address>${infoTeam.role}</address>
+        </div>
+    </div> `;
 }
