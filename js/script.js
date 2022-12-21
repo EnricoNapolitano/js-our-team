6fd4145f41cdcb102581c286bbda9f3c1c30f197
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-21 18:04:30 +0100

    team members array

--- js/script.js
+++ js/script.js
@@ -17,3 +17,41 @@ Prima la logica in italiano e poi traduciamo in codice.
 E ricordiamoci che console.log() è nostro amico!
 Buon lavoro!
 */
+
+//* DOMS ELEMENTS
+const teamMembers = document.getElementById('team-members');
+
+
+//*ARRAY WITH EVERY TEAM MEMBER INFO
+const infoTeam = [
+    {
+        name : 'Wayne Barnet',
+        role : 'Founder & CEO',
+        image : 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name : 'Angela Caroll',
+        role : 'Chief Editor',
+        image : 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name : 'Walter Gordon',
+        role : 'Office Manager',
+        image : 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name : 'Angela Lopez',
+        role : 'Social Media Manager',
+        image : 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name : 'Scott Estrada',
+        role : 'Developer',
+        image : 'scott-estrada-developer.jpg',
+    },
+    {
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        image : 'barbara-ramos-graphic-designerbarbara-ramos-graphic-designer.jpg',
+    }
+];
